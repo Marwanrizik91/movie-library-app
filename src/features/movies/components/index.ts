e73d987b272fecd5e36f43249143e2ef68237d9c
Marwan Rizik <59829281+Marwanrizik91@users.noreply.github.com>
2025-03-19 20:37:07 +0100

    refactor(movies): replace direct export with lazy loading for MovieCard.

--- src/features/movies/components/index.ts
+++ src/features/movies/components/index.ts
@@ -1,5 +1,6 @@
 import { lazy } from "react";
 export { default as MovieList } from "./MovieList";
 export { default as MovieModal } from "./MovieModal";
-export { default as MovieCard } from "./MovieCard";
+
+export const MovieCard = lazy(() => import("./MovieCard"));
 export const MovieHome = lazy(() => import("./MovieHome"));
