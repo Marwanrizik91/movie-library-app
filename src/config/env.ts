e34cc0c7123c9ca4c1cbe8756c3db6bd248d315f
Marwan Rizik <59829281+Marwanrizik91@users.noreply.github.com>
2025-03-20 09:59:05 +0100

    Refactor: Update env.ts
    Update env.ts to use vite env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,7 @@
 export const API_CONFIG = {
   baseUrl:
     import.meta.env.VITE_API_BASE_URL ||
-    (process.env.NODE_ENV === "production"
+    (import.meta.env.MODE === "production"
       ? "https://movie-library-api-i5f2.onrender.com/api/movies"
       : "http://localhost:3000/api/movies"),
 };
